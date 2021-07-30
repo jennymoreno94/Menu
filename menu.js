d875f2b573ecfee8342d2c4646cd364692c6910f
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-07-29 23:19:34 -0500

    se agrega menu.js

--- menu.js
+++ menu.js
@@ -1,2 +1,12 @@
 let circulo = document.getElementById("circulo");
 
+function Menu() {
+    for (i = 0; i < 10 ; i++ ) {
+        let circuloPequeno = document.createElement("div")
+        circuloPequeno.classList.add("circulo-pequeno")
+        circuloPequeno.classList.add(`num${i+1}`)
+        circulo.appendChild(circuloPequeno)
+    }   
+}
+
+document.onload = Menu()
