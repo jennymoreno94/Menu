d5e403822abcd3f8f64d6231fff76ff123ba32db
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-07-31 22:43:51 -0500

    lineas

--- menu.js
+++ menu.js
@@ -5,7 +5,11 @@ function Menu() {
         let circuloPequeno = document.createElement("div")
         circuloPequeno.classList.add("circulo-pequeno")
         circuloPequeno.classList.add(`num${i+1}`)
+        let linea = document.createElement("div")
+        linea.classList.add("linea")
+        linea.classList.add(`linea${i+1}`)
         circulo.appendChild(circuloPequeno)
+        circuloPequeno.appendChild(linea)
     }   
 }
 
